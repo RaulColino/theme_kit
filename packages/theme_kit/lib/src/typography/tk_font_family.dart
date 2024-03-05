5d188bd492f323dc4fb372162437eda33c87905c
rcs <r.colinos.2016@alumnos.urjc.es>
2024-03-05 17:34:10 +0100

    small changes in README

--- packages/theme_kit/lib/src/typography/tk_font_family.dart
+++
@@ -1,8 +0,0 @@
-class TkFontFamily {
-  const TkFontFamily._(this.name);
-  final String name;
-
-  static const TkFontFamily dufanthe = TkFontFamily._("Dufanthe");
-  static const TkFontFamily kumbhSans = TkFontFamily._("KumbhSans");
-  static const TkFontFamily inter = TkFontFamily._("Inter");
-}
