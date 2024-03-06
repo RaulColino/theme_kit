122e3a61f619e4dd444d5c0057a34cdff7de405a
rcs <r.colinos.2016@alumnos.urjc.es>
2024-03-06 02:40:56 +0100

    Small USAGE.md change

--- packages/devest_ui/lib/src/typography/dv_font_family.dart
+++
@@ -1,8 +0,0 @@
-class DvFontFamily {
-  const DvFontFamily._(this.name);
-  final String name;
-
-  static const DvFontFamily dufanthe = DvFontFamily._("Dufanthe");
-  static const DvFontFamily kumbhSans = DvFontFamily._("KumbhSans");
-  static const DvFontFamily inter = DvFontFamily._("Inter");
-}
